dd9bbdc0c8aeb42b1db6698be5b9322460816716
John Bates <jbates@zendesk.com>
2020-07-22 11:41:59 -0700

    Update

--- brew.sh
+++ brew.sh
@@ -39,8 +39,6 @@ main() {
   brew tap "caskroom/cask";
   brew tap "caskroom/fonts";
   brew tap "caskroom/versions";
-  # brew tap "homebrew/boneyard";
-  # brew tap "homebrew/completions";
   brew tap "homebrew/core";
   brew tap "homebrew/services";
   brew tap "homebrew/versions";
@@ -58,7 +56,10 @@ main() {
   notify "Java JDKs";
   # The jce-unlimited-strength-policy package is no longer required as of JDK v1.8.0.161.
   # Use the AdoptOpenJDK.net builds of the JDK instead of the Oracle ones.
-  brew cask install "adoptopenjdk8";
+  brew cask install "AdoptOpenJDK/openjdk/adoptopenjdk8";
+  brew cask install "AdoptOpenJDK/openjdk/adoptopenjdk11";
+  brew cask install "AdoptOpenJDK/openjdk/adoptopenjdk12";
+  brew cask install "AdoptOpenJDK/openjdk/adoptopenjdk13";
 
   if [[ ! -d "/usr/local/sbin" ]]; then
     >&2 echo "Your sudo password is required to create /usr/local/sbin";
@@ -83,36 +84,49 @@ main() {
   brew install colordiff;
   brew install coreutils;
   brew install csshx;
+  brew install ctop;
   brew install curl --with-nghttp2;
   brew install dependency-check;
   brew install diceware;
+  brew install ditaa;
   brew install docker;
   brew install docker-credential-helper; # for docker-credential-osxkeychain
   brew install dockutil;
   brew install enchant;
+  brew install findutils;
+  brew install fswatch;
   brew install gawk;
   brew install gcviewer;
+  brew install geoipupdate;
+  brew install gettext;
   brew install gist;
   brew install git --with-curl --with-openssl --with-perl;
   brew install git-cal;
   # brew install git-extras;
   brew install git-secrets;
+  brew install giter8;
+  brew install gnu-units;
   brew install gnupg;
   brew install gnupg2;
+  brew install go;
+  brew install go-jira;
   brew install gpg-agent;
   brew install graphviz;
   brew install grc;
   brew install grep;
+  brew install grpc;
   brew install hadolint;
   brew install hr;
   brew install htop;
   brew install httpie;
   brew install hub;
+  brew install hunspell;
   brew install ivy; # for ivycp
   brew install jenv; # rbenv for Java
   brew install jfrog-cli-go;
   brew install jmeter --with-plugins;
   brew install jq; # requires Java to be installed first.
+  brew install kubernetes-cli;
   brew install less; # includes lesskey (MacOS doesn't for some weird reason)
   brew install lesspipe;
   brew install mas;
@@ -120,21 +134,29 @@ main() {
   brew install mercurial; # aka "hg".
   brew install midnight-commander;
   brew install mill;
+  # brew install minikube;
   brew install most;
   brew install multitail;
-  brew install mysql && brew services start mysql;
+  brew install mysql; # && brew services start mysql;
+  brew install mysql@5.7; # && brew services start mysql@5.7;
+  brew install nano;
   brew install npm;
-  brew install openssl;
+  brew install openssl@1.1;
   brew install p7zip;
   brew install packer;
+  brew install pandoc;
   brew install pinentry-mac && brew linkapps pinentry-mac;
   # brew install proguard;
   brew install pstree;
   brew install python;
   brew install pwgen;
   brew install rbenv;
+  brew install rbenv-default-gems;
   brew install reattach-to-user-namespace;
+  brew install redis;
   brew install rfcdiff; # required for github.com/lvc/installer-4j
+  brew install rlwrap;
+  brew install ruby-build;
   brew install safe-rm;
   brew install sbt; # requires Java to be installed first.
   brew install scala;
@@ -147,17 +169,27 @@ main() {
   brew install sqlite;
   # brew install sshrc"; # https://github.com/Russell91/sshrc (now in .dotfiles, + local mods)
   brew install sshuttle; # SSH auto-VPN
+  brew install starship;
   brew install terminal-notifier && brew linkapps terminal-notifier;
   brew install terraform;
+  brew install travis;
   brew install tree;
+  brew install unzip;
+  brew install vault; # Hashicorp Vault
   brew install wdiff; # required for github.com/lvc/installer-4j
   brew install wget;
   # brew install wrk;
   brew install xmlstarlet;
+  brew install xz;
+  brew install yamllint;
+  brew install yq;
+  brew install zip;
 
   brew install discoteq/discoteq/flock;
+  brew install github/gh/gh;
   brew install johanhaleby/kubetail/kubetail;
   brew install moul/moul/docker-diff;
+  brew install versent/taps/saml2aws;
   brew install wagoodman/dive/dive;
 
   notify "Command line completions";
@@ -177,13 +209,22 @@ main() {
   notify "Fonts";
   # Hack info: https://github.com/chrissimpkins/Hack
   brew cask install font-hack;
+  brew cask install font-hack-nerd-font-mono;
   brew cask install font-fira-code;
+  brew cask instlal font-go-mono;
+  brew cask instlal font-go-mono-nerd-font-mono;
   brew cask install font-inter-ui; # https://rsms.me/inter/
   brew cask install font-source-code-pro;
+  brew cask install font-sourcecodepro-nerd-font-mono;
+
+  # Alternate JVMs
+  brew cask install graalvm/tap/graalvm-ce-java11;
+  brew cask install graalvm/tap/graalvm-ce-lts-java11;
 
   notify "Other tool casks";
   brew cask install 1password;
-  brew cask install --appdir="/Applications" araxis-merge;
+  brew cask install aerial; # Screensaver
+  brew cask install --appdir="/Applications" araxis-merge; # Was broken, may need to download and install manually.
   brew cask install authy;
   brew cask install --appdir="/Applications" bitbar;
   brew cask install --appdir="/Applications" ccmenu;
@@ -194,7 +235,7 @@ main() {
   brew cask install gpg-suite;
   brew cask install jetbrains-toolbox; # Manage IntelliJ, etc.
   brew cask install key-codes;
-  brew cask install minikube;
+  brew cask install lastpass;
   brew cask install --appdir="/Applications" mysqlworkbench;
   brew cask install osxfuse;
   brew cask install --appdir="/Applications" paintbrush;
@@ -213,8 +254,10 @@ main() {
   # easy_install "pip"; # pip is preinstalled with python 2.7.9+ or 3.4+
   pip install Pygments;
   pip install json-spec;
+  pip install mintotp;
+  pip install truffleHog;
   pip install xml2rfc; # via https://xml2rfc.tools.ietf.org/
-  pip install yamllint;
+  # pip install yamllint; # using brew instead.
 
   # For HTTPie (https://github.com/jkbrzt/httpie), SNI (Server Name Indication)
   # support needs updated stuff for Python versions < 2.7.9
@@ -225,7 +268,7 @@ main() {
   mkdir -p "${HOME}/.jenv/versions";
 
   # If the ~/.jenv/versions folder is empty, add all of the current JDK installations into jenv.
-  if [ -z "$(find "${HOME}/.jenv/versions" -mindepth 1 -print -quit 2> /dev/null)" ]; then
+  if [ -z "$(find "${HOME}/.jenv/versions" -mindepth 1 -maxdepth 1 -print -quit 2> /dev/null)" ]; then
     while IFS= read -r -d $'\n' jdk; do
       if [ -d "${jdk}/Contents/Home/bin" ]; then
         jenv add "${jdk}/Contents/Home";
@@ -235,7 +278,7 @@ main() {
 
   # Default to Java 8 (1.8) for SBT.
   if [ ! -r "${HOME}/.jenv/version" ]; then
-    jenv global 1.8;
+    jenv global 11;
     jenv rehash;
   fi;
 }
