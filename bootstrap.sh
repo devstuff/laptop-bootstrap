aba5e6e86424cc586b906ab2371d7ceff9520f6b
John Bates <jbates@zendesk.com>
2020-07-26 13:15:51 -0700

    Update.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,18 +12,15 @@ notify() {
 }
 
 main() {
-  mkdir -p "${HOME}/bootstrap";
+  mkdir -p "${HOME}/bootstrap" && cd "${HOME}/bootstrap" && true;
   notify "Downloading and installing Homebrew, Dropbox, etc.";
-  curl -sSfL "https://raw.githubusercontent.com/devstuff/laptop-bootstrap/brew.sh";
-
-  notify "Installing Dropbox.";
-  brew cask install dropbox;
+  /bin/bash -c "$(curl -sSfL "https://raw.githubusercontent.com/devstuff/laptop-bootstrap/brew.sh")";
 
   # shellcheck disable=SC2034
   read -r -p "Login to Dropbox, press Enter/Return when synchronization is complete." response;
 
   # Exec the ~/Dropbox/bootstrap-private script (private work stuff).
-  "${HOME}/Dropbox/bootstrap-private";
+  "${HOME}/Dropbox/bootstrap/bootstrap-private";
 }
 
 main "$@";
